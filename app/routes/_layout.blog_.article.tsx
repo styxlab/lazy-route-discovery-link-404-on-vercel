3d407cf1fb4d4c2b2a93666175eaff73607b0bf7
styxlab <styxlab@localhost.com>
2025-02-24 12:17:16 +0100

    slow down article

--- app/routes/_layout.blog_.article.tsx
+++ app/routes/_layout.blog_.article.tsx
@@ -1,6 +1,15 @@
+import { useEffect } from "react";
+
 export const config = { runtime: 'node' }
 
 export default function Article() {
+    useEffect(() => {
+      setTimeout(() => {
+        console.log('Article page loaded after 2 seconds');
+      }, 6000);
+    }, []);
+
+
     return (
       <div className="flex h-screen items-center justify-center">
         <div className="flex flex-col items-center gap-16">
