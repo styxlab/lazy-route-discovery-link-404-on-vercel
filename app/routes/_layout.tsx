7a9b672f579958bc6d834cc2a8a50f3e954d532e
styxlab <styxlab@localhost.com>
2025-02-24 12:03:03 +0100

    add examples

--- app/routes/_layout.tsx
+++ app/routes/_layout.tsx
@@ -1,6 +1,8 @@
 import { Outlet } from '@remix-run/react'
 
-export default function Partner() {
+export const config = { runtime: 'edge' }
+
+export default function Layout() {
   return (
     <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
       <Outlet />
